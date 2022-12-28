56c9ec397226ee2b89f03ff108ddbe553138d77e
Nicu Dascalu <dnikku@gmail.com>
2022-12-28 15:34:45 +0100

    add component checkbox with fontawesome icons

--- src/assets/FontIcons.ts
+++ src/assets/FontIcons.ts
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import { faGithub, faMarkdown } from '@fortawesome/free-brands-svg-icons'
 import { faHouse, faGear, faUser, faList, faGraduationCap } from "@fortawesome/free-solid-svg-icons"
+import { faSquare, faSquareCheck } from "@fortawesome/free-regular-svg-icons"
 
 library.add(faGithub)
 library.add(faMarkdown)
@@ -16,5 +17,8 @@ library.add(faGear)
 library.add(faUser)
 library.add(faList)
 library.add(faGraduationCap)
+
+library.add(faSquare)
+library.add(faSquareCheck)
 
 export const FontIcon = FontAwesomeIcon
---
+++ src/components/Checkbox.vue
@@ -0,0 +1,38 @@
+<template>
+  <input type="checkbox"
+         :checked="modelValue"
+         @input="(event) => $emit('update:modelValue', (event.target as any)?.value)"
+         :id="id"
+         class="hidden"
+  />
+  <label :for="id">
+    <font-icon v-show="modelValue" icon="fa-regular fa-square-check" />
+    <font-icon v-show="!modelValue" icon="fa-regular fa-square" />
+    {{ label }}
+  </label>
+</template>
+
+<style scoped>
+.hidden {
+  display: none;
+}
+</style>
+
+
+<script lang="ts">
+// https://dev.to/maxwelladapoe/creating-a-custom-checkbox-component-in-vue-3-a-better-approach-2jjd
+
+export default {
+  props: {
+    modelValue: Boolean,
+    label: String,
+    id: String
+  },
+  emits: ['update:modelValue'],
+
+  methods: {
+
+  }
+
+}
+</script>
