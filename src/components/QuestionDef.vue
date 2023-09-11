ccf90ea33043f3fbca251249541f01ced22daa1e
Nicu Dascalu <dnikku@gmail.com>
2023-09-11 02:34:06 +0200

    render multiline question/answer

--- src/components/QuestionDef.vue
+++ src/components/QuestionDef.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="question">{{ value.nr }}: {{ value.text_de }}</div>
+  <div class="question">{{ value.nr }}:
+    <multiline :text="value.text_de"/>
+  </div>
   <div class="question-actions">
     <checkbox label="Show Answer" v-model="showAnswer"/>
-    <checkbox class="en" v-show="value.text_en" label="Hint EN" v-model="hintEN" />
+    <checkbox class="en" v-show="value.text_en" label="Hint EN" v-model="hintEN"/>
     <checkbox class="ro" v-show="value.text_ro" label="Hint RO" v-model="hintRO"/>
   </div>
-  <div class="question en" v-show="hintEN">{{ value.text_en }}</div>
-  <div class="question ro" v-show="hintRO">{{ value.text_ro }}</div>
+  <div class="question en" v-show="hintEN">
+    <multiline :text="value.text_en"/>
+  </div>
+  <div class="question ro" v-show="hintRO">
+    <multiline :text="value.text_ro"/>
+  </div>
 
-  <div class="answer" v-show="showAnswer">- {{ value.answer_de }}</div>
-  <div class="answer en" v-show="showAnswer && hintEN">- {{ value.answer_en }}</div>
-  <div class="answer ro" v-show="showAnswer && hintRO">- {{ value.answer_ro }}</div>
+  <div class="answer" v-show="showAnswer">
+    <multiline :text="value.answer_de"/>
+  </div>
+  <div class="answer en" v-show="showAnswer && hintEN">
+    <multiline :text="value.answer_en"/>
+  </div>
+  <div class="answer ro" v-show="showAnswer && hintRO">
+    <multiline :text="value.answer_ro"/>
+  </div>
   <div class="answer-actions" v-show="showAnswer">
     <button>Learn it</button>
     <button>Re-learn</button>
@@ -62,6 +74,7 @@
 import {ref} from "vue";
 import Checkbox from "@/components/Checkbox.vue";
 import type {Question} from "@/stores/questionnaire";
+import Multiline from "@/components/Multiline.vue";
 
 const props = defineProps<{
   value: Question
