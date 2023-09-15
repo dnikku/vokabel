8a229d8f3d232a6c4803e82a64c6312466dbf772
Nicu Dascalu <dnikku@gmail.com>
2023-09-15 23:48:23 +0200

    fix settings storage

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,15 +1,16 @@
 import {defineStore} from "pinia"
-import {useStorage} from '@vueuse/core'
+import {useLocalStorage} from '@vueuse/core'
 import {computed} from "vue";
 
 
 export const useSettingsStore = defineStore('settings', () => {
-    const showResolution = useStorage("show-resolution", false)
-    const pageSize = useStorage("page-size", 10)
+    const showResolution = useLocalStorage("settings:show-resolution", false)
+    const pageSize = useLocalStorage("settings:page-size", 10)
 
-    const resolution = computed(() =>
-        showResolution.value ? `(W x H: ${window.innerWidth}x${window.innerHeight})` : ""
-    )
+    const resolution = computed(() => {
+        console.info(`show-resolution: ${showResolution.value}`)
+        return showResolution.value ? `(W x H: ${window.innerWidth}x${window.innerHeight})` : ""
+    })
 
     return {
         pageSize: pageSize,
